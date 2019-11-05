b6c56a995c29e4a5d467cd18ed45aac38f31e1c7
unknown <1114802868@qq.com>
2019-11-05 18:54:55 +0800

    coding

--- sf.v
+++ sf.v
@@ -4,3 +4,73 @@ Require Export Coq.Strings.String.
 From Coq Require Import Logic.FunctionalExtensionality.
 From Coq Require Import Lists.List.
 Import ListNotations.
+
+Definition eqb_string (x y:string):bool:=
+  if string_dec x y then true else false.
+
+(* for review the usage of destruct *)
+Theorem plus_1_neq_0 : forall n : nat,
+  (n + 1) =? 0 = false.
+Proof.
+  intros n. destruct n as [| n'] eqn:E.
+  - reflexivity.
+  - reflexivity.   Qed.
+
+Check string_dec.
+Check 1<>1.
+
+Theorem eqb_string_refl : forall s : string, true = eqb_string s s.
+Proof. intros s. unfold eqb_string. destruct (string_dec s s) as [|Hs] eqn:E. Print left.
+  - reflexivity.
+  - destruct Hs. reflexivity.
+Qed.
+
+Lemma equality_commutes:
+  forall (a: bool) (b: bool), a = b -> b = a.
+Proof.
+  intros.
+  subst.
+  reflexivity.
+Qed.
+
+Theorem injection_ex1 : forall (n m o : nat),
+  [n; m] = [o; o] ->
+  [n] = [m].
+Proof.
+  intros n m o H.
+  injection H. intros H1 H2.
+  rewrite H1. rewrite H2. reflexivity.
+Qed.
+
+Theorem S_injective' : forall (n m : nat),
+  S n = S m ->
+  n = m.
+Proof.
+  intros n m H.
+  injection H. intros Hnm. apply Hnm.
+Qed.
+
+Theorem eqb_string_true_iff : forall x y : string,
+    eqb_string x y = true <-> x = y.
+Proof.
+   intros x y.
+   unfold eqb_string.
+   destruct (string_dec x y) as [|Hs].
+   - subst. split. reflexivity. reflexivity.
+   - split.
+     + intros contra. discriminate contra.
+     + intros H. rewrite <- H in Hs. destruct Hs. reflexivity.
+Qed.
+
+Theorem eqb_string_false_iff : forall x y : string,
+    eqb_string x y = false <-> x <> y.
+Proof.
+  intros x y. rewrite <- eqb_string_true_iff.
+  rewrite not_true_iff_false. reflexivity. Qed.
+
+
+Theorem false_eqb_string : forall x y : string,
+   x <> y -> eqb_string x y = false.
+Proof.
+  intros x y. rewrite eqb_string_false_iff.
+  intros H. apply H. Qed.
